3796a760f39eeab5aad11238588025e9b6972fe3
AlanMenTen <156022369+AlanMenTen@users.noreply.github.com>
2024-10-22 14:09:03 -0600

    First Version

---
+++ src/main/java/mx/gob/metepec/government_support_request/GovernmentSupportRequestApplication.java
@@ -0,0 +1,13 @@
+package mx.gob.metepec.government_support_request;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernmentSupportRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovernmentSupportRequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/gob/metepec/government_support_request/GovernmentSupportRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.gob.metepec.government_support_request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernmentSupportRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
